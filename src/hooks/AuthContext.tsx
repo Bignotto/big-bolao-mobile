b72e1f3d2465ae38dc2e4c94af03d18ac5d46d81
Thiago Bignotto <bignotto@gmail.com>
2022-05-13 06:19:47 -0300

    implemented authentication context in src/hooks/AuthContext.tsx

---
+++ src/hooks/AuthContext.tsx
@@ -0,0 +1,75 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { Alert } from "react-native";
+import { AuthSession } from "@supabase/supabase-js";
+import supabase from "../shared/services/supabase";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+interface IAuthContextData {
+  session: AuthSession | undefined | null;
+  signIn(email: string, password: string): Promise<void>;
+  signOut(): Promise<void>;
+}
+
+const AuthContext = createContext({} as IAuthContextData);
+
+function AuthProvider({ children }: AuthProviderProps) {
+  const [session, setSession] = useState<AuthSession | null | undefined>(
+    undefined
+  );
+
+  async function signOut() {
+    try {
+      let { error } = await supabase.auth.signOut();
+      if (error) return Alert.alert("something wrong in sing out");
+    } catch (error) {}
+  }
+
+  async function signIn(email: string, password: string) {
+    const { error, user } = await supabase.auth.signIn({ email, password });
+
+    if (error) {
+      //TODO: Implement AppError
+      //throw new AppError(error.message, 0);
+      throw new Error("something wrong in AuthContext signIn function");
+    }
+  }
+
+  //TODO: Implement Sign Up function
+
+  useEffect(() => {
+    try {
+      const fetchedSession = supabase.auth.session();
+      setSession(fetchedSession || undefined);
+
+      //await
+      (async () => {
+        //check saved session
+        const storedSession = await AsyncStorage.getItem("supabase.auth.token");
+        if (!storedSession) {
+          setSession((oldSession) =>
+            oldSession === undefined ? null : oldSession
+          );
+        }
+      })();
+    } catch (error) {
+      console.log("try catch error on userContext");
+    }
+
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      async (event, session) => {
+        console.log(
+          `Supabase auth event: ${event}, session user: ${session?.user?.email}`
+        );
+        setSession(session || undefined);
+      }
+    );
+
+    return () => {
+      authListener!.unsubscribe();
+    };
+  }, []);
+}
