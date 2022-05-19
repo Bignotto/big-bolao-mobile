1e25df0cd32255c789aef10dca7c48b0b71139dd
Thiago Bignotto <bignotto@gmail.com>
2022-05-19 07:13:17 -0300

    implemented register screen and user sign up with supabase

--- src/screens/RegisterAccount/index.tsx
+++ src/screens/RegisterAccount/index.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+import { Alert } from "react-native";
 import { Button } from "../../shared/components/Button";
 import Input from "../../shared/components/Input";
+import { AppError } from "../../shared/errors/AppError";
+import { useAuth } from "../../shared/hooks/AuthContext";
 
 import {
   Container,
@@ -14,6 +17,25 @@ import {
 } from "./styles";
 
 export default function RegisterAccountScreen() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+
+  const { signUp } = useAuth();
+
+  async function handleSubmit() {
+    if (password !== passwordConfirm)
+      return Alert.alert("As senhas não batem.");
+
+    try {
+      await signUp(email, password);
+    } catch (error) {
+      if (error instanceof AppError) Alert.alert(error.message);
+      console.log(error);
+    }
+  }
+
   return (
     <Container>
       <Header>
@@ -24,7 +46,12 @@ export default function RegisterAccountScreen() {
       </Header>
       <InputField>
         <InputLabel>Seu nome:</InputLabel>
-        <Input name="name" placeholder="seu nome completo" value={""} />
+        <Input
+          name="name"
+          placeholder="seu nome completo"
+          value={name}
+          onChangeText={(text) => setName(text)}
+        />
       </InputField>
       <InputField>
         <InputLabel>E-Mail:</InputLabel>
@@ -32,19 +59,28 @@ export default function RegisterAccountScreen() {
           name="email"
           keyboardType="email-address"
           placeholder="email@address.com"
-          value={""}
+          value={email}
+          onChangeText={(text) => setEmail(text)}
         />
       </InputField>
       <InputField>
         <InputLabel>Senha:</InputLabel>
-        <Input name="password" placeholder="senha segura" value={""} />
+        <Input
+          name="password"
+          placeholder="senha segura"
+          value={password}
+          secureTextEntry={true}
+          onChangeText={(text) => setPassword(text)}
+        />
       </InputField>
       <InputField>
         <InputLabel>Confirmação:</InputLabel>
         <Input
           name="passwordConfirm"
+          secureTextEntry={true}
           placeholder="confirme sua senha"
-          value={""}
+          value={passwordConfirm}
+          onChangeText={(text) => setPasswordConfirm(text)}
         />
       </InputField>
       <Footer>
@@ -56,7 +92,7 @@ export default function RegisterAccountScreen() {
           Você deve confirmar seu endereço de e-mail para poder fazer login no
           aplicativo.
         </FooterText>
-        <Button title="Criar conta!" />
+        <Button title="Criar conta!" onPress={handleSubmit} />
       </Footer>
     </Container>
   );
