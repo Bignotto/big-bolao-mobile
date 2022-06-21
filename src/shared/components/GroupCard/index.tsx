9b38368a1f7b350eda56b0bcd8e199feec04e7f2
Thiago Bignotto <bignotto@gmail.com>
2022-06-20 21:11:17 -0300

    save commit styleing group card component

--- src/shared/components/GroupCard/index.tsx
+++ src/shared/components/GroupCard/index.tsx
@@ -1,10 +1,33 @@
 import React from "react";
-import { Container } from "./styles";
+import {
+  Container,
+  InfoContainer,
+  RankingContainer,
+  GroupTitleContainer,
+  GroupInfoContainer,
+  RankingTitleText,
+  RankingPositionText,
+  GroupTitle,
+  GroupPointsText,
+  GroupFriendsText,
+} from "./styles";
 
 function GroupCard() {
   return (
     <Container>
-      <InfoContainer></InfoContainer>
+      <InfoContainer>
+        <GroupTitleContainer>
+          <GroupTitle>Bar do Zé</GroupTitle>
+        </GroupTitleContainer>
+        <GroupInfoContainer>
+          <GroupPointsText>35 pontos</GroupPointsText>
+          <GroupFriendsText>8 amigos</GroupFriendsText>
+        </GroupInfoContainer>
+      </InfoContainer>
+      <RankingContainer>
+        <RankingTitleText>Position</RankingTitleText>
+        <RankingPositionText>1</RankingPositionText>
+      </RankingContainer>
     </Container>
   );
 }
--- src/shared/components/GroupCard/styles.ts
+++ src/shared/components/GroupCard/styles.ts
@@ -1,5 +1,65 @@
+import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
-  flex: 1;
+  background-color: red;
+  flex-direction: row;
+  height: 75px;
+`;
+
+export const InfoContainer = styled.View`
+  width: 75%;
+  background-color: ${({ theme }) => theme.colors.shape};
+
+  border-top-left-radius: 8px;
+  border-bottom-left-radius: 8px;
+  justify-content: center;
+
+  padding-left: 8px;
+`;
+
+export const RankingContainer = styled.View`
+  width: 25%;
+  background-color: ${({ theme }) => theme.colors.background};
+
+  border-top-right-radius: 8px;
+  border-bottom-right-radius: 8px;
+
+  border-top-width: 3px;
+  border-top-color: ${({ theme }) => theme.colors.shape};
+
+  border-right-width: 3px;
+  border-right-color: ${({ theme }) => theme.colors.shape};
+
+  border-bottom-width: 3px;
+  border-bottom-color: ${({ theme }) => theme.colors.shape};
+`;
+
+export const GroupTitleContainer = styled.View``;
+
+export const GroupInfoContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-around;
+`;
+
+export const RankingTitleText = styled.Text``;
+
+export const RankingPositionText = styled.Text``;
+
+export const GroupTitle = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.black_italic};
+  font-size: ${RFValue(26)}px;
+  color: ${({ theme }) => theme.colors.text};
+`;
+
+export const GroupPointsText = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(14)}px;
+  color: ${({ theme }) => theme.colors.text};
+`;
+
+export const GroupFriendsText = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(14)}px;
+  color: ${({ theme }) => theme.colors.text};
 `;
