a52c1755b3da9a69a696c1c7f353bc857b1f096b
Thiago Bignotto <bignotto@gmail.com>
2022-06-24 06:59:32 -0300

    implementing getUserGroups in GroupContext

--- src/shared/hooks/GroupContext.tsx
+++ src/shared/hooks/GroupContext.tsx
@@ -3,6 +3,18 @@ import { Alert } from "react-native";
 import { AppError } from "../errors/AppError";
 import supabase from "../services/supabase";
 
+interface Group {
+  created_at: Date;
+  group: {
+    group_id: string;
+    name: string;
+  };
+  id: string;
+  user_id: string;
+  user_points: number;
+  user_rank: number;
+}
+
 interface GroupProviderProps {
   children: ReactNode;
   userId: string;
@@ -20,9 +32,22 @@ function GroupProvider({ children, userId }: GroupProviderProps) {
     try {
       const { data, error } = await supabase
         .from("user_groups")
-        .select("*,group_id(name)")
+        .select("*,group_id(group_id,name)")
         .eq("user_id", userId);
-      console.log({ data });
+
+      const groups: Group[] | undefined = data?.map((g) => ({
+        created_at: g.created_at,
+        group: {
+          group_id: g.group_id.group_id,
+          name: g.group_id.name,
+        },
+        id: g.id,
+        user_id: g.user_id,
+        user_points: g.user_points,
+        user_rank: g.user_rank,
+      }));
+
+      console.log({ groups });
     } catch (error) {
       if (error instanceof AppError) return Alert.alert(error.message);
       console.log(`unknown ERROR: ${error}`);
