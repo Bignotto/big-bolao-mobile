6f623e44a87e2f583a359523b0c611bdf29cccfa
Thiago Bignotto <bignotto@gmail.com>
2022-07-28 07:39:01 -0300

    styled header and info panel

--- src/screens/GroupDashboard/index.tsx
+++ src/screens/GroupDashboard/index.tsx
@@ -1,10 +1,52 @@
+import { useNavigation, useRoute } from "@react-navigation/native";
 import React from "react";
-import { Container, Texto } from "./styles";
+import { StatusBar } from "react-native";
+import { useTheme } from "styled-components";
+import BackButton from "../../shared/components/BackButton";
+import { Group } from "../../shared/hooks/GroupContext";
+import {
+  ButtonWrapper,
+  Container,
+  Content,
+  GroupTitle,
+  GroupTitleContainer,
+  Header,
+  HeaderTitle,
+  HeaderTopWrapper,
+} from "./styles";
+
+interface Params {
+  group: Group;
+}
 
 export default function GroupDashboard() {
+  const route = useRoute();
+  const { group } = route.params as Params;
+  const navigation = useNavigation();
+  const theme = useTheme();
+
   return (
     <Container>
-      <Texto>Group Dashboard Screen</Texto>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor={theme.colors.shape}
+      />
+      <Header>
+        <HeaderTopWrapper>
+          <ButtonWrapper>
+            <BackButton
+              onPress={() => navigation.goBack()}
+              color={theme.colors.text}
+            />
+          </ButtonWrapper>
+          <HeaderTitle>{group.group.name}</HeaderTitle>
+        </HeaderTopWrapper>
+      </Header>
+      <Content>
+        <GroupTitleContainer>
+          <GroupTitle>{group.group.name}</GroupTitle>
+        </GroupTitleContainer>
+      </Content>
     </Container>
   );
 }
--- src/screens/GroupDashboard/styles.ts
+++ src/screens/GroupDashboard/styles.ts
@@ -3,12 +3,47 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  background-color: yellow;
-  justify-content: center;
-  align-items: center;
+  background-color: ${({ theme }) => theme.colors.background};
 `;
 
-export const Texto = styled.Text`
-  font-size: ${RFValue(18)}px;
-  color: black;
+export const Header = styled.View`
+  height: 110px;
+  background-color: ${({ theme }) => theme.colors.shape};
+  padding: 24px 24px;
+`;
+
+export const HeaderTopWrapper = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 12px;
+`;
+
+export const ButtonWrapper = styled.View`
+  margin-top: 4px;
+`;
+
+export const HeaderTitle = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.medium};
+  color: ${({ theme }) => theme.colors.text};
+  font-size: ${RFValue(32)}px;
+  margin-left: 12px;
+`;
+
+export const Content = styled.View`
+  padding: 0 24px;
+  margin-top: -20px;
+`;
+export const GroupTitleContainer = styled.View`
+  height: 48px;
+  background-color: ${({ theme }) => theme.colors.success};
+  align-items: center;
+  justify-content: center;
+  border-radius: 8px;
+  margin-bottom: 18px;
+`;
+
+export const GroupTitle = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.medium};
+  color: ${({ theme }) => theme.colors.text};
+  font-size: ${RFValue(28)}px;
 `;
