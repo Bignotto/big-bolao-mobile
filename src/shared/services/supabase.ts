415e39eaef1dbafe3680c1bdbcaab4d6c9be12e3
Thiago Bignotto <bignotto@gmail.com>
2022-10-04 06:31:40 -0300

    save commit

--- src/shared/services/supabase.ts
+++ src/shared/services/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import Config from "../../../Config";
+import { Config } from "../../../Config";
 
 export const supabase = createClient(
   Config.SUPABASE_URL!,
