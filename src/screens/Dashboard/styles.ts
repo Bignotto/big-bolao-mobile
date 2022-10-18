d12d6aa167a7fc298d5613ca52c127b492a57ef2
Thiago Bignotto <bignotto@gmail.com>
2022-10-18 06:05:32 -0300

    fixed dashboard screen padding

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -5,6 +5,8 @@ import styled from "styled-components/native";
 export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.background};
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
 export const ContentText = styled.Text`
@@ -12,4 +14,14 @@ export const ContentText = styled.Text`
   color: ${({ theme }) => theme.colors.text};
   font-family: ${({ theme }) => theme.fonts.regular};
   font-size: ${RFValue(16)}px;
+`;
+
+export const GroupListWrapper = styled.View`
+  flex: 1;
+  padding-left: 24px;
+  padding-right: 24px;
+`;
+
+export const Footer = styled.View`
+  padding: 24px;
 `;
